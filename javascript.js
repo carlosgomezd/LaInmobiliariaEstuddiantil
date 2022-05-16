2e0a4201836370fff4947e50b020e8d3e2b1ac09
Carlos Gómez <cgomezde43@alumnes.ub.edu>
2022-05-16 02:10:03 +0200

    Hide dels divs i començar a mostrar divs prop.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
-
 //localStorage.setItem("dataActived","false");
 const numero_pisos = 10;
 const users = [];
 var num_users = 0;
-
 
 function canviarText1() {
   var b = JSON.parse(localStorage["resultat1"]);
@@ -12,12 +10,15 @@ function canviarText1() {
   var h = b.habitacions;
   var p = b.preu;
   var s = b.superficie;
+  var car = b.carrer;
   document.getElementById("_im1").innerHTML = i;
   document.getElementById("_hab1").innerHTML = h;
   document.getElementById("_p1").innerHTML = p;
   document.getElementById("_c1").innerHTML = c;
   document.getElementById("_s1").innerHTML = s;
+  document.getElementById("_car1").innerHTML = car;
 }
+
 function canviarText2() {
   var b = JSON.parse(localStorage["resultat2"]);
   var c = b.ciutat;
@@ -25,11 +26,13 @@ function canviarText2() {
   var h = b.habitacions;
   var p = b.preu;
   var s = b.superficie;
+  var car = b.carrer;
   document.getElementById("_im2").innerHTML = i;
   document.getElementById("_hab2").innerHTML = h;
   document.getElementById("_p2").innerHTML = p;
   document.getElementById("_c2").innerHTML = c;
   document.getElementById("_s2").innerHTML = s;
+  document.getElementById("_car2").innerHTML = car;
 }
 
 function canviarText3() {
@@ -39,11 +42,13 @@ function canviarText3() {
   var h = b.habitacions;
   var p = b.preu;
   var s = b.superficie;
+  var car = b.carrer;
   document.getElementById("_im3").innerHTML = i;
   document.getElementById("_hab3").innerHTML = h;
   document.getElementById("_p3").innerHTML = p;
   document.getElementById("_c3").innerHTML = c;
   document.getElementById("_s3").innerHTML = s;
+  document.getElementById("_car3").innerHTML = car;
 }
 
 function getBusqueda() {
@@ -66,6 +71,7 @@ function getBusqueda() {
   }
   window.location.href = "./Resultado_cerca.html";
   var busqueda = new pisos(-1, strCiutat, strImm, strHab, preu, sup);
+  busqueda.carrer = "";
   localStorage["busq"] = JSON.stringify(busqueda);
 }
 
@@ -80,32 +86,52 @@ function showPisos() {
 }
 
 function pisos(id, ciutat, immoble, habitacions, preu, superficie) {
+  const carrer = [
+    "Pau Casals",
+    "Jacint Verdaguer",
+    "Pompeu Fabra",
+    "Joan Maragall",
+    "Jaume Balmes",
+    "Lluís Companys",
+    "Àngel Guimerà",
+    "	Francesc Macià",
+    "Jaume I",
+    "	Joan Miró",
+  ];
   this.id = id;
   this.ciutat = ciutat;
   this.immoble = immoble;
   this.habitacions = habitacions;
   this.preu = preu;
   this.superficie = superficie;
+  this.carrer = carrer[Math.floor(Math.random() * carrer.length)];
 }
 
+function crearDiv() {
+  var link = document.createElement("a");
+  var nLista = document.createElement("li");
+  var card = document.createElement("div");
+  var el = document.getElementById("llista");
+  link.setAttribute('href', './finalResult.html');
+  card.setAttribute('class','card');
+  el.appendChild(nLista);
+  nLista.appendChild(link);
+  link.appendChild(card);
+
+
+}
 
 function hide() {
   var h2 = document.getElementById("hide2");
   var h3 = document.getElementById("hide3");
-  if (localStorage["nRes"] === 3) {
-    h2.style.display = "block";
-    h3.style.display = "block";
-  }
-  if (localStorage["nRes"] === 2) {
-    h2.style.display = "block";
-    h3.style.display = "none";
-  }
-  if (localStorage["nRes"] === 1) {
+  if (localStorage["nRes"] == 1) {
     h2.style.display = "none";
     h3.style.display = "none";
   }
+  if (localStorage["nRes"] == 2) {
+    h3.style.display = "none";
+  }
 }
-
 
 function compararCerca() {
   const lst = [];
@@ -145,15 +171,14 @@ function compararCerca() {
   localStorage["nRes"] = lst.length;
 }
 
-
 function initData() {
-  if (localStorage.getItem("dataActived") === null){
+  if (localStorage.getItem("dataActived") === null) {
     console.log("ENTRA EN EL INITDATA");
     localStorage["exec"] = false;
     localStorage["dataActived"] = "false";
     console.log("VALOR LOCAL STORAGE: " + JSON.parse(localStorage["exec"]));
-    if (localStorage.getItem("dataActived") === "false"){
-      console.log("INiciando datos");
+    if (localStorage.getItem("dataActived") === "false") {
+      console.log("Iniciando datos");
       crearPisos();
       localStorage["exec"] = true;
       localStorage["numUsers"] = "0";
@@ -161,7 +186,6 @@ function initData() {
     }
   }
 }
-
 
 function crearPisos() {
   const ciu = ["Barcelona", "Girona", "Tarragona", "Lleida"];
@@ -194,7 +218,6 @@ function crearPisos() {
   localStorage.setItem("nPisos", piso.length.toString());
 }
 
-
 function getValorPreu() {
   var slider = document.getElementById("pr");
   var output = document.getElementById("preu");
@@ -203,7 +226,6 @@ function getValorPreu() {
     output.innerHTML = this.value;
   };
 }
-
 
 function getValorSuperficie() {
   var slider = document.getElementById("sup");
@@ -214,8 +236,7 @@ function getValorSuperficie() {
   };
 }
 
-
-function User(name, surname, username, password){
+function User(name, surname, username, password) {
   this.name = name;
   this.surname = surname;
   this.username = username;
@@ -223,76 +244,71 @@ function User(name, surname, username, password){
   this.saved_pisos = [];
 }
 
-
-function registerUser () {
+function registerUser() {
   var name = document.getElementById("register_user_name").value;
   var surname = document.getElementById("register_user_surname").value;
-  var username = document.getElementById("register_user_mail").value.toLowerCase();
+  var username = document
+    .getElementById("register_user_mail")
+    .value.toLowerCase();
   var password = document.getElementById("register_user_password").value;
-  var r_password = document.getElementById("register_user_password_repeat").value;
+  var r_password = document.getElementById(
+    "register_user_password_repeat"
+  ).value;
 
   // Comprovem si falta algun camp
-  if (name && surname && username  && password && r_password){
-
-    if (password === r_password){
-    
-      if (this.getUser(username) === null){
-        var user = new User (name, surname, username, password, r_password);
+  if (name && surname && username && password && r_password) {
+    if (password === r_password) {
+      if (this.getUser(username) === null) {
+        var user = new User(name, surname, username, password, r_password);
 
         var numUser = parseInt(localStorage.getItem("numUsers")) + 1;
         localStorage["numUsers"] = numUser.toString();
         localStorage.setItem("user" + numUser.toString(), JSON.stringify(user));
 
         console.log("NUMERO DE CLIENTS: " + localStorage.getItem("numUsers"));
-        window.location.href="./main.html";
+        window.location.href = "./main.html";
         localStorage["actualUser"] = JSON.stringify(user);
-      }
-
-      else{
+      } else {
         window.alert("Ja existeix un usuari amb aquest compte");
-        window.location.href="./register.html";
+        window.location.href = "./register.html";
       }
-    }
-    else{
+    } else {
       window.alert("Els contrasenyes introduides no coincideixen");
-      window.location.href="./register.html";
+      window.location.href = "./register.html";
     }
-  }
-  else{
+  } else {
     window.alert("Algun dels camps no es troba complert");
-    window.location.href="./register.html";
+    window.location.href = "./register.html";
   }
 }
 
-function existUser (username, password) {
+function existUser(username, password) {
   console.log("Usuaris: " + localStorage["numUsers"]);
   var numUsuaris = parseInt(localStorage.getItem("numUsers"));
-  
-  for (let i = 0; i < numUsuaris; i++){
+
+  for (let i = 0; i < numUsuaris; i++) {
     var user = JSON.parse(localStorage.getItem("user" + (i + 1).toString()));
 
-    if (user.username === username && user.password === password){
+    if (user.username === username && user.password === password) {
       return true;
     }
-    
   }
 
   return false;
 }
 
-
 /**
  * Aquesta funció ens recupera l'usuari que el seu nom coincideixi
- * @param {} username 
- * @returns 
+ * @param {} username
+ * @returns
  */
-function getUser (username) {
+function getUser(username) {
   var numUsuaris = parseInt(localStorage.getItem("numUsers"));
 
-  for (let i = 0; i < numUsuaris; i++){
+  for (let i = 0; i < numUsuaris; i++) {
     var user = JSON.parse(localStorage.getItem("user" + (i + 1).toString()));
 
-    if (user.username === username){
+    if (user.username === username) {
       return user;
     }
   }
@@ -300,25 +316,21 @@ function getUser (username) {
   return null;
 }
 
-
 /**
  * Realitza el logun de l'usuari introduit al login.html
  */
-function login_user () {
+function login_user() {
   username = document.getElementById("user_mail").value;
   password = document.getElementById("user_password").value;
 
-  if (this.existUser(username, password)){
-
-
+  if (this.existUser(username, password)) {
     localStorage.setItem("actualUser", JSON.stringify(this.getUser(username)));
-    localStorage.setItem("initUser", "true"); 
-    window.location.href="./main.html";
-    document.getElementById("loginButton").innerHTML = 'El meu perfil';
+    localStorage.setItem("initUser", "true");
+    window.location.href = "./main.html";
+    document.getElementById("loginButton").innerHTML = "El meu perfil";
     localStorage["exec"] = true;
-  }
-  else{
+  } else {
     window.alert("L'usuari introduit no existeix");
-    window.location.href="./login.html";
+    window.location.href = "./login.html";
   }
 }
